0b66a9a5a890385bcb2164f93c8d50d21216dd54
Jakub Michalewicz <jakubmichalewicz1996@gmail.com>
2021-02-10 22:11:42 +0100

    Initial commit

---
+++ RTS_AoV/Source/RTS_AoV/RTS_AoVGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "RTS_AoVGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class RTS_AOV_API ARTS_AoVGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
