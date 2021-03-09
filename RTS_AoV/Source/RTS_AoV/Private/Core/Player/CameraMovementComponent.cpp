01d98e40fa911fa3cfda45fa770c2e645974a495
Jakub Michalewicz <jakubmichalewicz1996@gmail.com>
2021-03-10 00:24:33 +0100

    fix

--- RTS_AoV/Source/RTS_AoV/Private/Core/Player/CameraMovementComponent.cpp
+++ RTS_AoV/Source/RTS_AoV/Private/Core/Player/CameraMovementComponent.cpp
@@ -12,17 +12,17 @@ UCameraMovement_Component::UCameraMovement_Component()
 	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
 	// off to improve performance if you don't need them.
 	PrimaryComponentTick.bCanEverTick = true;
-	PrimaryComponentTick.bStartWithTickEnabled = true;
+	//PrimaryComponentTick.bStartWithTickEnabled = true;
 
-	// Set Default attributes
-	bDisableCameraMovement = false;
-	DefaultMovementSpeed = 15.0;
-	MovementSpeedModifier = 1.0;
-	DefaultZoomLength = 1350.0;
-	DeltaArm = 150.0;
-	DeltaRotation = FRotator(7.15, 0.0, 0.0);
-	MaxZoom = 300.0; // The closest the camera can get from the map
-	MinZoom = 3000.0; // The farthest the camera can get from the map
+	//// Set Default attributes
+	//bDisableCameraMovement = false;
+	//DefaultMovementSpeed = 15.0;
+	//MovementSpeedModifier = 1.0;
+	//DefaultZoomLength = 1350.0;
+	//DeltaArm = 150.0;
+	//DeltaRotation = FRotator(7.15, 0.0, 0.0);
+	//MaxZoom = 300.0; // The closest the camera can get from the map
+	//MinZoom = 3000.0; // The farthest the camera can get from the map
 }
 
 
@@ -30,8 +30,8 @@ UCameraMovement_Component::UCameraMovement_Component()
 void UCameraMovement_Component::BeginPlay()
 {
 	Super::BeginPlay();
-	CameraOwner = Cast<ACameraPawn>(GetOwner()); // Populates the owner of this component
-	PlayerControllerRef = Cast<APlayerController>(UGameplayStatics::GetPlayerController(GetWorld(), 0)); // Populates PlayerControllerRef
+	//CameraOwner = Cast<ACameraPawn>(GetOwner()); // Populates the owner of this component
+	//PlayerControllerRef = Cast<APlayerController>(UGameplayStatics::GetPlayerController(GetWorld(), 0)); // Populates PlayerControllerRef
 }
 
 
@@ -41,66 +41,66 @@ void UCameraMovement_Component::TickComponent(float DeltaTime, ELevelTick TickTy
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 }
 
-//Methods
-//Getters
-FVector UCameraMovement_Component::GetCameraLocation()
-{
-	return CameraOwner->GetActorLocation();
-}
-FVector UCameraMovement_Component::GetFaceDirection()
-{
-	return CameraOwner->GetActorForwardVector();
-}
-
-FRotator UCameraMovement_Component::GetCameraRotation()
-{
-	return CameraOwner->GetActorRotation();
-}
-
-//Setters
-float UCameraMovement_Component::SetMovementSpeedModifier(float ModifierAmount)
-{
-	return MovementSpeedModifier = ModifierAmount;
-}
-
-float UCameraMovement_Component::SetMovementSpeed(float MovementSpeedAdjustment)
-{
-	return DefaultMovementSpeed = MovementSpeedAdjustment;
-}
-
-bool UCameraMovement_Component::SetCameraDisable(bool bDisableCamera)
-{
-	return bDisableCameraMovement = bDisableCamera;
-}
-
-//Basic Movement Controlls
-/*Movement Control - W A S D*/
-void UCameraMovement_Component::BasicMovementControl(float AxisValueX, float AxisValueY)
-{
-	// Set Variables used in movement calculation
-	float MovementValueX;
-	float MovementValueY;
-	FVector MovementX;
-	FVector MovementY;
-
-	if (!IsCameraDisable())
-	{
-		if (AxisValueX != 0)
-		{
-			MovementValueX = AxisValueX * GetCurrentMovementSpeed() * GetSpeedModifier();
-			MovementX = FVector(MovementValueX, 0, 0);
-
-			CameraOwner->AddActorLocalOffset(MovementX, true);
-		}
-		else if (AxisValueY != 0)
-		{
-			MovementValueY = AxisValueY * GetCurrentMovementSpeed() * GetSpeedModifier();
-			MovementY = FVector(0, MovementValueY, 0);
-
-			CameraOwner->AddActorLocalOffset(MovementY, true);
-		}
-	}
-}
+////Methods
+////Getters
+//FVector UCameraMovement_Component::GetCameraLocation()
+//{
+//	return CameraOwner->GetActorLocation();
+//}
+//FVector UCameraMovement_Component::GetFaceDirection()
+//{
+//	return CameraOwner->GetActorForwardVector();
+//}
+//
+//FRotator UCameraMovement_Component::GetCameraRotation()
+//{
+//	return CameraOwner->GetActorRotation();
+//}
+//
+////Setters
+//float UCameraMovement_Component::SetMovementSpeedModifier(float ModifierAmount)
+//{
+//	return MovementSpeedModifier = ModifierAmount;
+//}
+//
+//float UCameraMovement_Component::SetMovementSpeed(float MovementSpeedAdjustment)
+//{
+//	return DefaultMovementSpeed = MovementSpeedAdjustment;
+//}
+//
+//bool UCameraMovement_Component::SetCameraDisable(bool bDisableCamera)
+//{
+//	return bDisableCameraMovement = bDisableCamera;
+//}
+//
+////Basic Movement Controlls
+///*Movement Control - W A S D*/
+//void UCameraMovement_Component::BasicMovementControl(float AxisValueX, float AxisValueY)
+//{
+//	// Set Variables used in movement calculation
+//	float MovementValueX;
+//	float MovementValueY;
+//	FVector MovementX;
+//	FVector MovementY;
+//
+//	if (!IsCameraDisable())
+//	{
+//		if (AxisValueX != 0)
+//		{
+//			MovementValueX = AxisValueX * GetCurrentMovementSpeed() * GetSpeedModifier();
+//			MovementX = FVector(MovementValueX, 0, 0);
+//
+//			CameraOwner->AddActorLocalOffset(MovementX, true);
+//		}
+//		else if (AxisValueY != 0)
+//		{
+//			MovementValueY = AxisValueY * GetCurrentMovementSpeed() * GetSpeedModifier();
+//			MovementY = FVector(0, MovementValueY, 0);
+//
+//			CameraOwner->AddActorLocalOffset(MovementY, true);
+//		}
+//	}
+//}
 
 //void UCameraMovement_Component::PanCamera(float RotationAmount)
 //{
--- RTS_AoV/Source/RTS_AoV/Public/Core/Player/CameraMovementComponent.h
+++ RTS_AoV/Source/RTS_AoV/Public/Core/Player/CameraMovementComponent.h
@@ -25,92 +25,92 @@ public:
 	// Called every frame
 	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
 
-	/*Get Attached Pawn*/
-	UPROPERTY(Transient, EditAnywhere, BluepriReadWrite)
-	ACameraPawn* CameraOwner;
+//	/*Get Attached Pawn*/
+//	UPROPERTY(Transient, EditAnywhere, BluepriReadWrite)
+//	ACameraPawn* CameraOwner;
+//
+//	/*Characeter controller refrence*/
+//	UPROPERTY(BlueprintReadWrite)
+//	APlayerController* PlayerControllerRef;
+//
+//protected:
+//	//Atributes
+//
+//	//Bools
+//	/*Is movement disable?*/
+//	UPROPERTY(Category = "Movement Variables: Bools", BlueprintReadOnly)
+//	bool bDisableCameraMovement;
+//
+//	//MovementSpeeds
+//	/*How fast the Camera Moves?*/
+//	UPROPERTY(Category = "Movement Variables: Movement Speeds", BlueprintReadOnly)
+//	float DefaultMovementSpeed;
+//
+//	/*Modifies the default movement speed when called*/
+//	UPROPERTY(Category = "Movement Variables: Movement Speeds", BlueprintReadOnly)
+//	float MovementSpeedModifier;
+//
+//	//Camera Direction
+//	/*Gets Forward Direction of Camera Pawn*/
+//	UPROPERTY(Category = "Movement Variables: Movement Speeds", BlueprintReadOnly)
+//	FVector FaceDirection;
+//
+//	//Zoom
+//	/*Default CameraArm Length*/
+//	UPROPERTY(Category = "Movement Variables: Zoom", BlueprintReadOnly)
+//	float DefaultZoomLength;
+//
+//	/*Amount CameraArm Changes*/
+//	UPROPERTY(Category = "Movement Variables: Zoom", BlueprintReadOnly)
+//	float DeltaArm;
+//
+//	/*Amount The Rotation Changes*/
+//	UPROPERTY(Category = "Movement Variables: Zoom", BlueprintReadOnly)
+//	FRotator DeltaRotation;
+//
+//	/*Farthest Camera can get on zoom out*/
+//	UPROPERTY(Category = "Movement Variables: Zoom", BlueprintReadOnly)
+//	float MaxZoom;
+//
+//	/*Closes Camera can get on zoom in*/
+//	UPROPERTY(Category = "Movement Variables: Zoom", BlueprintReadOnly)
+//	float MinZoom;
 
-	/*Characeter controller refrence*/
-	UPROPERTY(BlueprintReadWrite)
-	APlayerController* PlayerControllerRef;
-
-protected:
-	//Atributes
-
-	//Bools
-	/*Is movement disable?*/
-	UPROPERTY(Category = "Movement Variables: Bools", BlueprintReadOnly)
-	bool bDisableCameraMovement;
-
-	//MovementSpeeds
-	/*How fast the Camera Moves?*/
-	UPROPERTY(Category = "Movement Variables: Movement Speeds", BlueprintReadOnly)
-	float DefaultMovementSpeed;
-
-	/*Modifies the default movement speed when called*/
-	UPROPERTY(Category = "Movement Variables: Movement Speeds", BlueprintReadOnly)
-	float MovementSpeedModifier;
-
-	//Camera Direction
-	/*Gets Forward Direction of Camera Pawn*/
-	UPROPERTY(Category = "Movement Variables: Movement Speeds", BlueprintReadOnly)
-	FVector FaceDirection;
-
-	//Zoom
-	/*Default CameraArm Length*/
-	UPROPERTY(Category = "Movement Variables: Zoom", BlueprintReadOnly)
-	float DefaultZoomLength;
-
-	/*Amount CameraArm Changes*/
-	UPROPERTY(Category = "Movement Variables: Zoom", BlueprintReadOnly)
-	float DeltaArm;
-
-	/*Amount The Rotation Changes*/
-	UPROPERTY(Category = "Movement Variables: Zoom", BlueprintReadOnly)
-	FRotator DeltaRotation;
-
-	/*Farthest Camera can get on zoom out*/
-	UPROPERTY(Category = "Movement Variables: Zoom", BlueprintReadOnly)
-	float MaxZoom;
-
-	/*Closes Camera can get on zoom in*/
-	UPROPERTY(Category = "Movement Variables: Zoom", BlueprintReadOnly)
-	float MinZoom;
-
-public:
-	//Methods
-
-	//Getters
-	UFUNCTION(BlueprintCallable, BlueprintPure, Category = "Camera Speed")
-	FORCEINLINE float GetCurrentMovementSpeed() { return DefaultMovementSpeed; }
-
-	UFUNCTION(BlueprintCallable, BlueprintPure, Category = "Camera Speed")
-	FORCEINLINE float GetSpeedModifier() { return MovementSpeedModifier; }
-
-	UFUNCTION(BlueprintCallable, BlueprintPure, Category = "Camera Speed")
-	FORCEINLINE bool IsCameraDisable() { return bDisableCameraMovement; }
-
-	UFUNCTION(BlueprintCallable, BlueprintPure, Category = "Camera Location")
-	FVector GetCameraLocation();
-
-	UFUNCTION(BlueprintCallable, BlueprintPure, Category = "Camera Direction")
-	FVector GetFaceDirection();
-
-	UFUNCTION(BlueprintCallable, BlueprintPure, Category = "Camera Speed")
-	FRotator GetCameraRotation();
-
-	//Setters
-	UFUNCTION(BlueprintCallable, Category = "Camera Speed")
-	float SetMovementSpeedModifier(float ModifierAmount);
-
-	UFUNCTION(BlueprintCallable, Category = "Camera Speed")
-	float SetMovementSpeed(float MovementSpeedAdjustment);
-
-	UFUNCTION(BlueprintCallable, Category = "Camera Speed")
-	bool SetCameraDisable(bool bDisableCamera);
-
-	//Utility
-	UFUNCTION(BlueprintCallable, Category = "Camera Movement")
-	virtual void BasicMovementControl(float AxisValueX, float AxisValueY);
+//public:
+//	//Methods
+//
+//	//Getters
+//	UFUNCTION(BlueprintCallable, BlueprintPure, Category = "Camera Speed")
+//	FORCEINLINE float GetCurrentMovementSpeed() { return DefaultMovementSpeed; }
+//
+//	UFUNCTION(BlueprintCallable, BlueprintPure, Category = "Camera Speed")
+//	FORCEINLINE float GetSpeedModifier() { return MovementSpeedModifier; }
+//
+//	UFUNCTION(BlueprintCallable, BlueprintPure, Category = "Camera Speed")
+//	FORCEINLINE bool IsCameraDisable() { return bDisableCameraMovement; }
+//
+//	UFUNCTION(BlueprintCallable, BlueprintPure, Category = "Camera Location")
+//	FVector GetCameraLocation();
+//
+//	UFUNCTION(BlueprintCallable, BlueprintPure, Category = "Camera Direction")
+//	FVector GetFaceDirection();
+//
+//	UFUNCTION(BlueprintCallable, BlueprintPure, Category = "Camera Speed")
+//	FRotator GetCameraRotation();
+//
+//	//Setters
+//	UFUNCTION(BlueprintCallable, Category = "Camera Speed")
+//	float SetMovementSpeedModifier(float ModifierAmount);
+//
+//	UFUNCTION(BlueprintCallable, Category = "Camera Speed")
+//	float SetMovementSpeed(float MovementSpeedAdjustment);
+//
+//	UFUNCTION(BlueprintCallable, Category = "Camera Speed")
+//	bool SetCameraDisable(bool bDisableCamera);
+//
+//	//Utility
+//	UFUNCTION(BlueprintCallable, Category = "Camera Movement")
+//	virtual void BasicMovementControl(float AxisValueX, float AxisValueY);
 
 	//UFUNCTION(BlueprintCallable, Category = "Camera Movement")
 	//virtual void PanCamera(float RotationAmount);
