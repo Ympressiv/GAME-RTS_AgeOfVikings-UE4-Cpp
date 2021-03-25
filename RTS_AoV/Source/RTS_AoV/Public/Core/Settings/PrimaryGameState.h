aafe276bffaa126247771d0e4d7af73b26d07981
Jakub Michalewicz <jakubmichalewicz1996@gmail.com>
2021-03-25 01:15:53 +0100

    Starting: DateTime

--- RTS_AoV/Source/RTS_AoV/Public/Core/Settings/PrimaryGameState.h
+++ RTS_AoV/Source/RTS_AoV/Public/Core/Settings/PrimaryGameState.h
@@ -4,14 +4,59 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/GameStateBase.h"
+#include "Misc/DateTime.h"
 #include "PrimaryGameState.generated.h"
 
-/**
- * 
- */
 UCLASS()
 class RTS_AOV_API APrimaryGameState : public AGameStateBase
 {
 	GENERATED_BODY()
 	
+public:
+	/*Constructor to set default values*/
+	APrimaryGameState();
+
+protected:
+	/*Begin Play*/
+	virtual void BeginPlay() override;
+
+public:
+	/*Tick*/
+	virtual void Tick(float DeltaTime) override;
+
+//Attributes
+protected:
+	/*Controler REF - Do later*/
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Calendar");
+	int Years;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Calendar");
+	int Months
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Calendar");
+	int Days;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Calendar");
+	int DayOfWeek;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Calendar");
+	int DayCounter;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Calendar");
+	int Hours;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Calendar");
+	int Minutes;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Calendar");
+	int Seconds;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Game Speed Setting");
+	float GameSpeed;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Game Speed Setting");
+	float GameTime;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Game Speed Setting");
+	FDateTime DateTimeStruct;
 };
