c9fbd4464c66e573d238b50a0dca56ecbab3cdf1
Jakub Michalewicz <jakubmichalewicz1996@gmail.com>
2021-04-03 03:33:51 +0200

    Character

--- RTS_AoV/Source/RTS_AoV/Private/Core/Units/AOV_UnitMaster.cpp
+++ RTS_AoV/Source/RTS_AoV/Private/Core/Units/AOV_UnitMaster.cpp
@@ -1,14 +1,9 @@
 // Copyright Epic Games, Inc. All Rights Reserved.
 
 #include "/Robocze_ProjektyGier/GAME-RTS_AgeOfVikings-UE4-Cpp/RTS_AoV/Source/RTS_AoV/Public/Core//Units/AOV_UnitMaster.h"
-#include "UObject/ConstructorHelpers.h"
-#include "Camera/CameraComponent.h"
 #include "Components/DecalComponent.h"
 #include "Components/CapsuleComponent.h"
 #include "GameFramework/CharacterMovementComponent.h"
-#include "GameFramework/PlayerController.h"
-#include "GameFramework/SpringArmComponent.h"
-#include "HeadMountedDisplayFunctionLibrary.h"
 #include "Materials/Material.h"
 #include "Engine/World.h"
 
